5fd69f0137e2e6c115dca36d9d6ccaf1bf9c675b
Sebastian <sebastianmurphy516@gmail.com>
2024-07-16 12:29:55 -0400

    Other versions added

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function generateFizzBuzz(fizz, buzz, end) {
     let numbers = [];
     for(i = 1; i <= end; i++){
         if (i % fizz == 0 && i % buzz == 0) {
-            numbers.push("Fizzbuzz");
+            numbers.push("FizzBuzz");
         } else if (i % fizz == 0) {
             numbers.push("Fizz");
         } else if (i % buzz == 0) {
@@ -27,12 +27,47 @@ function generateFizzBuzz(fizz, buzz, end) {
     }
     return numbers;
 }
+
+function generateFizzBuzzB(fizz, buzz, end) {
+    let arr = [];
+    for (i =1; i <= end; i++) {
+        fizzer = (i % fizz == 0);
+        buzzer = (i % buzz == 0);
+        switch (true) {
+            case fizzer && buzzer: {
+                arr.push("FizzBuzz");
+                break;
+            }
+            case fizzer: {
+                arr.push("Fizz");
+                break;
+            }
+            case buzzer: {
+                arr.push("Buzz");
+                break;
+            }
+            default: {
+                arr.push(i)
+                break;
+            } 
+        }
+    }
+    return arr;
+}
+function generateFizzBuzzC(fizz, buzz, end) {
+    let arr = [];
+    for (let i = 1; i <=end; i++) {
+        arr.push(( i % fizz == 0 ? "Fizz" : "") + ( i % buzz == 0 ? "Buzz" : "") || i);
+    }
+    return arr;
+}
+
 function displayFizzBuzz(numbers) {
     // Array index starts at zero so final element is length-1
     let templateCols = "";
     for(let i=0; i < numbers.length; i++) {
         let classname = "";
-        if(numbers[i] == "Fizzbuzz") {
+        if(numbers[i] == "FizzBuzz") {
             let classname = "fizzbuzz";
         templateCols += `<div class="col ${classname}">${numbers[i]},</div>`;
         } else if (numbers[i] == "Fizz") {
